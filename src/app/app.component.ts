195148575ab9ff0aa49fcfdde4e6e8078e5bfffb
MastanBalaji <mastanbalaji@gmail.com>
2020-08-05 11:38:59 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'final';
+  navbar=[
+    {
+      name:'Tirumala Package',
+      id:''
+    },
+    {
+      name:'South Tour Packages',
+      id:''
+    },
+    {
+      name:'Special Tour Packages',
+      id:''
+    },
+    {
+      name:'Car Rentals',
+      id:''
+    },
+    {
+      name:'Tickets',
+      id:''
+    },
+    {
+      name:'Hotel Bookings',
+      id:''
+    },
+  ]
 }
